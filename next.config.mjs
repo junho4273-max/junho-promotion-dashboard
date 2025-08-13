273bf2259b9ae2567269ae49439580b6e973c28e
junho4273-max <junho4273@gmail.com>
2025-08-13 16:34:46 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true }
+  // 선택: 빌드 시 ESLint 검사 생략 (지금은 문제 없지만 편의상)
+  eslint: { ignoreDuringBuilds: true },
 };
 export default nextConfig;
